08891d262f1f599e1fa62ade35217599b47cb674
MEDINA, SEBASTIAN ESTEBAN <105304709+SEEDBASTIAN@users.noreply.github.com>
2024-11-30 11:33:03 -0300

    update requirements.txt

--- LoginVulnerabilidad/wsgi.py
+++ LoginVulnerabilidad/wsgi.py
@@ -7,10 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+from django.core.wsgi import get_wsgi_application
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LoginVulnerabilidad.settings')
-
 application = get_wsgi_application()
